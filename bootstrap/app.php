11fbad612e187b61b31e1e1431c676f984a375d8
Nu'man Nasyar MZ <numannasyarmz11@gmail.com>
2025-01-03 01:03:49 +0700

    Feat: registering middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,12 +6,17 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+            'admin' => \App\Http\Middleware\Admin::class,
+            'user' => \App\Http\Middleware\User::class,
+            'walsan' => \App\Http\Middleware\Walsan::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
