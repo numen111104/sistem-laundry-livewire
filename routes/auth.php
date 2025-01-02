dcac4e17ee6929d8cd08a336e1d456a5c41e2c19
Nu'man Nasyar MZ <numannasyarmz11@gmail.com>
2025-01-03 01:05:33 +0700

    Feat: managing routes for each roles

--- routes/auth.php
+++ routes/auth.php
@@ -4,7 +4,6 @@ use Livewire\Volt\Volt;
 use App\Livewire\Auth\Login;
 use App\Livewire\Auth\Register;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\VerifyEmailController;
 
 Route::middleware('guest')->group(function () {
     Volt::route('login', Login::class)
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,26 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Livewire\Admin\DashboardAdmin;
 use App\Livewire\Dashboard;
+use App\Livewire\User\DashboardUser;
+use Illuminate\Support\Facades\Route;
+use App\Livewire\Walsan\DashboardWalsan;
 
-Route::name('user::')->middleware('auth')->group(function () {
-    Route::get('/', Dashboard::class)->name('dashboard');
+
+Route::group(['middleware' => ['auth', 'user'], 'as' => 'user::'], function () {
+    Route::get('/', DashboardUser::class)->name('dashboardUser');
+});
+
+Route::group(['middleware' => ['auth', 'admin'], 'as' => 'admin::'], function () {
+    Route::prefix('admin')->group(function () {
+        Route::get('/', DashboardAdmin::class)->name('dashboardAdmin');
+    });
+});
+
+Route::group(['middleware' => ['auth', 'walsan'], 'as' => 'walsan::'], function () {
+    Route::prefix('walsan')->group(function () {
+        Route::get('/', DashboardWalsan::class)->name('dashboardWalsan');
+    });
 });
 
 require __DIR__ . '/auth.php';
