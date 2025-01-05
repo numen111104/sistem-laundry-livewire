af81db82fc0b7caca234e42368e3f3c459a2c84f
Nu'man Nasyar MZ <numannasyarmz11@gmail.com>
2025-01-05 20:22:40 +0700

    Feat: These headers are changed to livewire components

---
+++ app/Livewire/Layouts/Headers/HeaderUser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Layouts\Headers;
+
+use Livewire\Component;
+
+class HeaderUser extends Component
+{
+    public function render()
+    {
+        return view('livewire.layouts.headers.header-user');
+    }
+}
---
+++ app/Livewire/Layouts/Headers/HeaderWalsan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Layouts\Headers;
+
+use Livewire\Component;
+
+class HeaderWalsan extends Component
+{
+    public function render()
+    {
+        return view('livewire.layouts.headers.header-walsan');
+    }
+}
